8f203a02d0a4834f5cfca06c8a3ff5ffcdb591e1
hitesh jangir <itshitesh18@gmail.com>
2025-03-17 12:53:43 +0530

    proxy update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express()
 
-app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://3.110.244.96:8000/", // HTTP API endpoint
-      changeOrigin: true,
-      secure: false, // Ignores SSL errors (if applicable)
-      pathRewrite: { "^/api": "" }, // Optional: Rewrite API path
-    })
-  );
 
 app.set('trust proxy', 1);
 
